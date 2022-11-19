e62a9c183dd51ac1b5c26398f9e5bc00743577d3
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-11-19 06:34:43 +0600

    hero section added and responsive done

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./HeroSection.css";
+import chirImage from "../../assets/images/chair.png";
+
+const HeroSection = () => {
+  return (
+    <section id="hero_section" className="container mx-auto h-[80vh] flex items-center justify-center">
+      <div className="hero">
+        <div className="hero-content flex-col md:flex-row lg:flex-row">
+          {/* Hero left image */}
+          <div className="">
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6">
+              Dentistry, also known as dental medicine and oral medicine, is the
+              branch of medicine focused on the teeth, gums, and mouth.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+          {/* Hero left image */}
+
+          {/* Hero right content */}
+
+          <img
+            src={chirImage}
+            className="rounded-lg hidden md:block lg:block w-[70%] shadow-2xl h-full"
+          />
+
+          {/* Hero right content end */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/utilities/Routes.js
+++ src/utilities/Routes.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '../pages/Home/Home'
 import Main from './Main'
 
 const Routes = () => {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <Main />
+            element: <Main />,
+            children: [
+              {
+                path: '/',
+                element: <Home />
+              }
+            ]
         }
     ])
   return (
